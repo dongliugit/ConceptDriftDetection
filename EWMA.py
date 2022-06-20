2501eec6514de671655c3ee3998f2cf8363be143
dongliugit <83126473+dongliugit@users.noreply.github.com>
2022-06-20 17:31:53 +0200

    Add files via upload

---
+++ EWMA.py
@@ -0,0 +1,100 @@
+# Dong Liu
+# created : 2022/2/10 10:54
+
+
+############ Ausgangsgößen(Prognose des Gewichts) aus Regressionsaufabe müssen zu Klassifikation wechseln
+############ 100 datasets----> standard deviation ----> prediction(Prognose des Gewichts) corret or not
+
+
+
+'''
+每次添加一个 Value-----预测正确Xt为0， 错误为1，上面第二个来编
+
+'''
+
+
+
+import os
+import sys
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+from sklearn.neighbors import KDTree
+import random
+import jprops
+import statistics as stat
+from matplotlib import pyplot
+
+
+
+class ECDD_DETEKTION(object):
+    def __init__(self):
+        self.threshold = 0              #Lt
+        self.count = 0
+        self.pr = 0.0                           #P0t
+        self.sd = 0.0                           #sigma Xt
+        self.expf = 0.0                         #lambda
+        self.z = 0.0                            #Zt
+        self.sdz = 0.0                          #sigma Zt
+
+
+    def ecdd(self, values, expf=0.2, warmUp=0):     # values--->List für Prognose des Gewicht, .tolist()
+        """
+        ECDD algorithm
+        """
+        self.expf = expf                        ### parameter λ(hier=0.2), how much weight is given to more recent data compared with older data
+
+        result = list()                         ### list für conceptdrift Informationen(count, estimator,conceptdirft)
+        if warmUp > 0:
+            for i in range(warmUp):
+                self.ecddStep(values[i])
+                r = (self.count, self.z, 0)     ### nur zählen und berechnen, keine Conceptdrift Detektion von [0 - warmUp)
+                result.append(r)
+        else:
+            for i in range(warmUp, len(values), 1): ##Detektion von warmUp bis zum Ende
+                self.ecddStep(values[i])
+                self.threshold=1.17+7.56*self.pr-21.24*self.pr**3+112.12*self.pr**5-987.23*self.pr**7
+                ###control limit
+
+
+
+                bound = self.pr + self.threshold * self.sdz
+                dr = 1 if self.z > bound else 0     ##  0->no CD, 1->confirm CD
+                if dr==1:
+                    result.append(self.count)
+                else:
+                    continue
+
+
+
+
+        return result
+
+    def ecddStep(self, val):
+        """
+        ECDD one step exponential forecast
+        """
+        self.count += 1
+        self.pr = ((self.count-1) * self.pr) / (self.count) + val / (self.count)          # p0t
+        self.sd = self.pr * (1.0 - self.pr)                     # sigma xt
+        e = 1.0 - self.expf
+        self.sdz = math.sqrt(self.sd * self.expf * (1.0 - e ** (2 * self.count)) / (2.0 - self.expf))
+        self.z = e * self.z + self.expf * val
+
+
+
+    # def ecddSave(self, fpath): ???????????????????????????????
+    #     """
+    #     save ECDD algorithm state
+    #     """
+    #     ws = dict()
+    #     ws["count"] = self.count
+    #     ws["pr"] = self.pr
+    #     ws["expf"] = self.expf
+    #     ws["z"] = self.z
+    #     saveObject(ws, fpath)????????????????????????????????
+
+'''
+Value (Xt（1/0）) als input
+'''
